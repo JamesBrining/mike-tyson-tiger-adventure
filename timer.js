e240ba3e1ce004da86263dc078e675734b21bab7
James Brining <james.brining@streets-heaver.com>
2024-03-23 14:04:23 +0000

    (feat) added spinning hourglass to timer

--- timer.js
+++ timer.js
@@ -1,7 +1,4 @@
 function updateTimer() {
     const timeElapsed = Math.floor((Date.now() - startTime) / 1000); // Time in seconds
     document.getElementById('timer').innerText = timeElapsed + 's';
-    const hourglass = document.getElementById('hourglass')
-    const isRotated = hourglass.style.transform === 'rotate(45deg)'
-    hourglass.style.transform = isRotated ? '' : 'rotate(45deg)'
   }
